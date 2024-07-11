0229289bcca7138f515ef3d326dd5314ed292a6b
Raman Sahi <sahiraman7@gmail.com>
2024-07-11 21:01:25 +0530

    posts

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 
 const db = require("./config/mongoose");
+
+// used for session cookie
+const session = require("express-session");
+
+const passport = require("passport");
+
+const passportLocal = require("./config/passport-local-strategy");
+
+const MongoStore = require("connect-mongo")(session);
 app.use(expressLayouts);
 
 // extract style and scripts from sub pages into the layout
@@ -20,15 +29,46 @@ app.use(cookieParser());
 
 app.use(express.static("./assets"));
 
-// use express router
-
-app.use("/", require("./routes"));
-
 // set up the view engine
 
 app.set("view engine", "ejs");
 
 app.set("views", "./views");
+// mongo store is used to store
+// the session cookie in the db
+app.use(
+  session({
+    name: "codeial",
+    // TODO change the secret before deployment in production mode
+
+    secret: "blahsomething",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: new MongoStore(
+      {
+        mongooseConnection: db,
+        autoRemove: "disabled",
+      },
+
+      function (err) {
+        console.log(err || "connect-mongodb setup ok");
+      }
+    ),
+  })
+);
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+
+app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
   if (err) {
